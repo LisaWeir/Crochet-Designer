29e9a4178f1d88bc5fd1561dfb3494b466dbdd2d
Lisa Weir <lisamweir1@gmail.com>
2014-04-28 06:18:57 -0700

    Added LWCell class

---
+++ LWCell.m
@@ -0,0 +1,13 @@
+//
+//  LWCell.m
+//  Crochet Designer
+//
+//  Created by Lisa Weir on 4/28/14.
+//  Copyright (c) 2014 Lisa Weir. All rights reserved.
+//
+
+#import "LWCell.h"
+
+@implementation LWCell
+
+@end
